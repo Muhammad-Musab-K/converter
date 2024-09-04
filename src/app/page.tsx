67b08ca196a0c6231c48ac03f21fcfbba34ea4c7
Muahmmad-Musab-K <kmusab824@gmail.com>
2024-09-04 14:07:01 +0500

    converter done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaFilePdf } from "react-icons/fa6";
 import axios from 'axios';
 import { Progress } from "@/components/ui/progress";
 import { BiSolidFileJson } from "react-icons/bi";
 import { useToast } from "@/hooks/use-toast";
-import { AiOutlineClose } from "react-icons/ai"; 
+import { AiOutlineClose } from "react-icons/ai";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 interface ApiResponse {
   x: number;
@@ -20,6 +22,13 @@ export default function Home() {
   const [progress, setProgress] = useState<number>(0);
   const { toast } = useToast();
   const [inJson, setInJson] = useState<ApiResponse[]>([]);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
 
   const convertPDFintoJSON = async (file: File) => {
     if (!file) {
@@ -34,8 +43,7 @@ export default function Home() {
     formData.append('file', file);
 
     setLoading(true);
-    setProgress(0); // Reset progress
-
+    setProgress(0);
     try {
       const response = await axios.post("https://resume-parser-gules.vercel.app/upload2", formData, {
         headers: {
@@ -80,10 +88,12 @@ export default function Home() {
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>): void => {
     e.preventDefault();
+    e.stopPropagation();
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>): void => {
     e.preventDefault();
+    e.stopPropagation();
     handleFileUpload(e);
   };
 
@@ -96,53 +106,59 @@ export default function Home() {
   return (
     <main className="w-screen h-screen text-slate-600 flex min-h-screen justify-center flex-col items-center p-5 lg:p-24">
       <h1 className="font-extrabold text-4xl mb-10 text-center">Convert PDF into JSON online for free</h1>
-      <div className="w-full md:w-4/5 shadow-lg rounded-md p-4">
-        <div
-          className="flex gap-4 md:gap-6 flex-col justify-center items-center m-auto border-2 rounded-sm border-dashed p-6 border-slate-300"
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
-          {!loading && inJson.length === 0 && (
-            <>
-              <FaFilePdf className="text-6xl text-orange-600" />
-              <h4 className="text-xl text-center">Drag and drop your document here to upload</h4>
-              <input
-                type="file"
-                accept=".pdf"
-                className="hidden"
-                id="file-upload"
-                onChange={handleFileUpload}
-              />
-              <label htmlFor="file-upload" className="bg-orange-400 text-white py-2 px-4 rounded-lg shadow hover:bg-orange-500 cursor-pointer">
-                Select from device
-              </label>
-              <p className="text-slate-300 text-sm">
-                Up to 100 MB for PDF files.
-              </p>
-            </>
-          )}
-          {loading && (
-            <div className="w-80 flex flex-col gap-3 items-center">
-              <BiSolidFileJson className="text-6xl text-yellow-600" />
-              <span className="w-80 flex gap-1">
-                <Progress value={progress} max={100} />
-                <p className="text-center mt-2 text-xs">{progress}%</p>
-              </span>
-            </div>
-          )}
-          {!loading && inJson.length > 0 && (
-            <div className="w-full">
-              <div className="flex justify-between items-center">
-                <h2 className="text-2xl font-bold">Parsed JSON Data</h2>
-                <AiOutlineClose className="text-xl cursor-pointer" onClick={handleReset} />
+      <Card className="w-full md:w-4/5 pt-6">
+        <CardContent>
+          <div
+            className="flex gap-4 md:gap-6 flex-col justify-center items-center m-auto border-2 rounded-sm border-dashed p-6 border-slate-300"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            {!loading && inJson.length === 0 && (
+              <>
+                <FaFilePdf className="text-6xl text-orange-600" />
+                <h4 className="text-xl text-center">Drag and drop your document here to upload</h4>
+                <input
+                  type="file"
+                  accept=".pdf"
+                  className="hidden"
+                  id="file-upload"
+                  ref={fileInputRef}
+                  onChange={handleFileUpload}
+                />
+                <Button
+                  onClick={handleButtonClick}
+                  variant="selection"
+                  size="lg">
+                  Select from device
+                </Button>
+                <p className="text-slate-300 text-sm">
+                  Up to 100 MB for PDF files.
+                </p>
+              </>
+            )}
+            {loading && (
+              <div className="w-80 flex flex-col gap-3 items-center">
+                <BiSolidFileJson className="text-6xl text-yellow-600" />
+                <span className="w-80 flex gap-1">
+                  <Progress value={progress} max={100} />
+                  <p className="text-center mt-2 text-xs">{progress}%</p>
+                </span>
               </div>
-              <p className="bg-gray-100 p-4 rounded-md overflow-x-auto max-h-96">
-                {JSON.stringify(inJson)}
-              </p>
-            </div>
-          )}
-        </div>
-      </div>
+            )}
+            {!loading && inJson.length > 0 && (
+              <div className="w-full">
+                <div className="flex justify-between items-center">
+                  <h2 className="text-2xl font-bold"> JSON Data</h2>
+                  <AiOutlineClose className="text-xl cursor-pointer" onClick={handleReset} />
+                </div>
+                <p className="bg-gray-100 p-4 rounded-md overflow-x-auto max-h-96">
+                  {JSON.stringify(inJson)}
+                </p>
+              </div>
+            )}
+          </div>
+        </CardContent>
+      </Card>
       {!loading && inJson.length === 0 && (
         <div>
           <p className="text-slate-300 text-sm text-center mt-8">
